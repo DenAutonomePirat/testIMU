2f2278c3dcc1c34db719e3dfb1f48bcf97e9ec61
DenAutonomePirat <appelsinkanon@gmail.com>
2016-09-15 10:13:58 +0200

    leaking all sorts of shit

--- ingest.go
+++ ingest.go
@@ -20,6 +20,7 @@ func ingest() {
 		log.Fatal(err)
 	}
 	defer arduino.Close()
+
 	for {
 		reader := bufio.NewReader(arduino)
 		token, err := reader.ReadBytes('\x0a')
@@ -31,7 +32,31 @@ func ingest() {
 		if err != nil {
 			fmt.Println(".")
 		}
-		fmt.Printf("%s\n", self.Marshal())
-		fmt.Println(reader.Buffered())
+
 	}
+}
+
+type Boat struct {
+	Id          int32   `json:"id"`
+	Rudder      int32   `json:"rudder"`
+	Depth       float32 `json:"depth"`
+	MainSail    int32   `json:"mainsail"`
+	Jib         int32   `json:"jib"`
+	Volts       float32 `json:"volts`
+	Amperes     float32 `json:"amperes"`
+	JoulesTotal float32 `json:"joules_total"`
+	joulesTrip  float32 `json:"joules_trip"`
+	Heading     float32 `json:"heading"`
+	Pitch       float32 `json:"pitch,omitempty"`
+	Roll        float32 `json:"roll"`
+}
+
+func (b *Boat) Marshal() *[]byte {
+	encoded, _ := json.Marshal(b)
+	return &encoded
+}
+
+func NewBoat() *Boat {
+	b := Boat{}
+	return &b
 }
